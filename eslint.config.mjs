964de0215f8e4fde45a4d80a087e611b3f985b3f
abhijith-p-subash <abhijith.p.subash@gmail.com>
2024-12-15 20:14:13 +0530

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  // The files to lint
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+
+  // Specify globals
+  { languageOptions: { globals: globals.browser } },
+
+  // Use recommended configurations
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
+  // Add ignores to specify files/folders to exclude from linting
+  {
+    ignores: ["node_modules", "dist", "build", "*.min.js"],
+  },
 ];
