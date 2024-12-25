752e74e3f7a06ce4cb4af738a1fce075725097b9
abhijith-p-subash <abhijith.p.subash@gmail.com>
2024-12-25 14:44:44 +0530

    prep for publish

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import firebaseConfig from "./config/firebase.config";
 import { fireAuthService } from "./modules/fireAuth.service";
 import { fireStoreDatabaseService } from "./modules/fireStoreDatabase.service";
 import { fireStorageService } from "./modules/fireStorage.service";
+import { Task, TaskResponse, WhereCondition, TaskOptions } from "./utils/task";
+
+export type {
+  Task,
+  TaskResponse,
+  WhereCondition,
+  TaskOptions,
+}
 
 export {
   firebaseConfig,
@@ -11,3 +19,5 @@ export {
 };
 
 // Author: Abhijith P Subash
+
+// flame-core
--- src/utils/task.ts
+++ src/utils/task.ts
@@ -5,7 +5,7 @@
  * @export
  * @interface WhereCondition
  */
-type WhereCondition = {
+export type WhereCondition = {
   [key: string]:
     | string
     | number
