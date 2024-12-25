cb33c2dfbf60d27fa2fc2dd8d1904f7af2d8cf01
abhijith-p-subash <abhijith.p.subash@gmail.com>
2024-12-25 07:59:10 +0530

    index. update

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,6 @@ import { fireAuthService } from "./modules/fireAuth.service";
 import { fireStoreDatabaseService } from "./modules/fireStoreDatabase.service";
 import { fireStorageService } from "./modules/fireStorage.service";
 
-firebaseConfig.initialize({
-  apiKey: "AIzaSyCpv_GfAtP_V5PUyWsvq3dYhJ_TYcfxUXI",
-  authDomain: "flame-core.firebaseapp.com",
-  projectId: "flame-core",
-  storageBucket: "flame-core.firebasestorage.app",
-  messagingSenderId: "694484863589",
-  appId: "1:694484863589:web:878f5ff8728e10505a548c",
-});
 
 export {
   firebaseConfig,
