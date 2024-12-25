f48fd724acb31c9efc7f8ba9cdb1cb2b9d1af4ee
abhijith-p-subash <abhijith.p.subash@gmail.com>
2024-12-25 13:21:58 +0530

    command removde

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-// Import Firebase configuration
 import firebaseConfig from "./config/firebase.config";
-
-// Import services for Firebase Authentication, Firestore Database, and Storage
 import { fireAuthService } from "./modules/fireAuth.service";
 import { fireStoreDatabaseService } from "./modules/fireStoreDatabase.service";
 import { fireStorageService } from "./modules/fireStorage.service";
 
-// Export configuration and services for external usage
 export {
   firebaseConfig,
   fireStoreDatabaseService,
